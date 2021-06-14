bf1465e2d35af07723508b48255a6d0f683d721a
Daria Petrova <o.mine.God@gmail.com>
2021-06-14 13:03:20 +0300

    Сделана прозрачность картинки, возможность скрывать лучшее предложение

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
 function changeTitleColor(item) {
   let child = item.querySelector('.room__title');
-  child.classList.toggle('clicked');
+  if ( child.classList.contains('clicked')) {
+    child.classList.add('double-clicked');
+  }
+  child.classList.add('clicked');
+}
+
+function changeImageOpacity(item) {
+  let child = item.querySelector('.room__image');
+  child.classList.add('clicked-image');
 }
 
 function changeRoomColor(item) {
@@ -13,14 +21,25 @@ function showMessage(item) {
   a.classList.remove('hidden');
 }
 
+function hiddenBestPrice(item) {
+  let child = item.querySelector('.room__bestprice.room-bestprice');
+  if(child) child.classList.add('hidden');
+}
+
 function setInfoPanel(item) {
   let temp1 = window.getComputedStyle(item.querySelector('.room__info.room-info'));
   let temp2 = window.getComputedStyle(item.querySelector('.room__item'));
-  let height = Number(temp1.getPropertyValue('height').substr(0,2)) + 
-               Math.ceil(Number(temp1.getPropertyValue('border-top-width').substr(0,4)))*2 +
+  let t = temp1.getPropertyValue('border-top-width')
+  if (t ==='1px') t='0.99px';
+  let lenghtString1 = temp1.getPropertyValue('height').indexOf('.');
+  let lenghtString2 = temp1.getPropertyValue('height').indexOf('p');
+  let lenghtString;
+  lenghtString1 ===(-1)?lenghtString = lenghtString2: lenghtString = lenghtString1; 
+  let height = Number(temp1.getPropertyValue('height').slice(0, lenghtString)) + 
+               Math.ceil(Number(t.substr(0,4)))*2 +
                Number(temp1.getPropertyValue('padding-top').substr(0,2)) +
                Number(temp2.getPropertyValue('padding-bottom').substr(0,2));
-  let a = item.querySelector('.room__gradient');
+  let a = item.querySelector('.room__gradient1');
   a.classList.add('reserved');
   let b = item.querySelector('.reserved');
   b.style.height = height +'px';
@@ -29,10 +48,10 @@ function setInfoPanel(item) {
     if (temp3.classList) {
       temp3.classList.remove('hidden');
       changeTitleColor(item);
+      changeImageOpacity(item);
+      hiddenBestPrice(item);
     }
   }
-  
-  
 }
 
 function reserveRoom() {
@@ -41,6 +60,7 @@ function reserveRoom() {
       let parent = t.closest('.room')
       changeTitleColor(parent);
       changeRoomColor(parent);
+
       //showMessage(parent)
       //console.log(parent.classList)
       //parent.classList.add('hidden')
