5ce553a5853da7b352a7369afd94070b1263a779
Daria Petrova <o.mine.God@gmail.com>
2021-06-26 18:32:22 +0300

    переделаны фукции

--- src/js/main.js
+++ src/js/main.js
@@ -1,107 +1,28 @@
-
-function changeTitleColor(item) {
-  let child = item.querySelector('.room__title');
-  if ( child.classList.contains('clicked')) {
-    child.classList.add('double-clicked');
-  } else {
-    child.classList.add('clicked');
-  }
-}
-
-function changeImageOpacity(item) {
-  let child = item.querySelector('.room__image');
-  child.classList.toggle('clicked-image');
-}
-
-function changeRoomColor(item) {
-    item.addEventListener('mouseleave', () =>  setInfoPanel(item), {once: true});
-};
-
-function showMessage(item) {
-  let a = item.querySelector('.reserved__message.hidden');
-  a.classList.remove('hidden');
-}
-
-function hiddenBestPrice(item) {
-  let child = item.querySelector('.room__bestprice.room-bestprice');
-  if(child) child.classList.toggle('hidden');
-}
-
-function setInfoPanel(item) {
-  let temp1 = window.getComputedStyle(item.querySelector('.room__info.room-info'));
-  let temp2 = window.getComputedStyle(item.querySelector('.room__item'));
-  let t = temp1.getPropertyValue('border-top-width')
-  if (t ==='1px') t='0.99px';
-  let lenghtString1 = temp1.getPropertyValue('height').indexOf('.');
-  let lenghtString2 = temp1.getPropertyValue('height').indexOf('p');
-  let lenghtString;
-  lenghtString1 ===(-1)?lenghtString = lenghtString2: lenghtString = lenghtString1; 
-  let height = Number(temp1.getPropertyValue('height').slice(0, lenghtString)) + 
-               Math.ceil(Number(t.substr(0,4)))*2 +
-               Number(temp1.getPropertyValue('padding-top').substr(0,2)) +
-               Number(temp2.getPropertyValue('padding-bottom').substr(0,2));
-  let a = item.querySelector('.room__reserved');
-  a.classList.add('reserved');
-  let b = item.querySelector('.reserved');
-  b.style.height = height +'px';
-  if (item.querySelector('.reserved__message.hidden')) {
-    const temp3 = item.querySelector('.reserved__message.hidden');
-    if (temp3.classList) {
-      temp3.classList.remove('hidden');
-      changeTitleColor(item);
-      changeImageOpacity(item);
-      hiddenBestPrice(item);
-    }
-  }
-}
-
 function reserveRoom() {
-  document.addEventListener('click', ({target: t}) => {
-    if (t.className === 'room-info__button') {
-      let parent = t.closest('.room');
-      if(parent.classList !==  'room selected'){
-        changeTitleColor(parent);
-        changeRoomColor(parent);
-        parent.classList.add('selected');
-      } 
-    }
-  })
-};
+  let targetElement = this.event.target
+  if (targetElement.closest('.room')) {
+    changeStyle(targetElement.closest('.room'))
+  }
+}
+
+function changeStyle(item) {
+  if (item.classList) {
+    item.classList.add('room_active')
+    item.addEventListener('mouseleave', () => {
+      item.classList.remove('room_active')
+      item.classList.add('room_reserved')
+    }, {once: true})
+  }
+}
 
 function cancelReserve() {
   document.addEventListener('click', ({target: t}) => {
-      if (t.className !== 'room-info__button' && t.className !== 'reserved__link') {
-        let parent = t.closest('.room.selected');
-        if (parent) {
-          cancelTitleColor(parent);
-          hiddenMessage(parent);
-          cancelInfoPanel(parent);
-          changeImageOpacity(parent);
-          hiddenBestPrice(parent)
-        }
+    if (t.className !== 'room__reserved-link') {
+      if (t.closest('.room.room_reserved')) {
+        t.closest('.room.room_reserved').classList.remove('room_reserved')
+      }
     }
   })
-};
-
-function cancelTitleColor(item) {
-  let child = item.querySelector('.room__title');
-  if ( child.classList.contains('clicked') && child.classList.contains('double-clicked')){
-    child.classList.remove('clicked');
-    child.classList.remove('double-clicked');
-  }
 }
 
-function cancelInfoPanel(item) {
-  if (item){
-    let a = item.querySelector('.room__reserved.reserved');
-    a.classList.remove('reserved'); 
-  }
-}
-
-function hiddenMessage(item) {
-  let a = item.querySelector('.reserved__message');
-  a.classList.add('hidden');
-}
-
-reserveRoom()
-cancelReserve()
+cancelReserve();
